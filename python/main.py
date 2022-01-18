5cb31260811a798560114bd6048e8798df58ae7d
toma09to <yamto0916@gmail.com>
2022-01-18 14:14:55 +0900

    py

--- python/main.py
+++ python/main.py
@@ -41,14 +41,19 @@ print("Enter the resolution of the graph")
 res = int(input())
 print("f(x)= ?")
 strInput = input()
+strInput2 = input()
 
 # string conversion
 strInput = re.sub(r'(?=sin|cos|tan|exp|floor)', 'math.', strInput)
 strInput = re.sub(r'\^', '**', strInput)
+strInput2 = re.sub(r'(?=sin|cos|tan|exp|floor)', 'math.', strInput2)
+strInput2 = re.sub(r'\^', '**', strInput2)
 
 # define f(x)
 def f(x):
     return eval(strInput)
+def g(x):
+    return eval(strInput2)
 
 # initialize image
 im = Image.new('RGB', (res,res), (255,255,255))
@@ -56,6 +61,7 @@ draw = ImageDraw.Draw(im)
 lineWidth = math.ceil(res / 500)
 
 fx = []
+gx = []
 
 # initialize graph
 draw.line([(0,res / 2), (res,res / 2)], fill='black', width=lineWidth)
@@ -78,5 +84,16 @@ for i in range(res):
     if i > 0:
         if fx[i-1] < 2 * res and fx[i-1] >= -res and fx[i] < 2 * res and fx[i] >= -res:
             draw.line([(i-1,fx[i-1]), (i,fx[i])], fill='blue', width=lineWidth)
+
+for i in range(res):
+    x = (i * 2 - res) * displayXRange / (res * 2.0)
+    try:
+        y = round((displayYRange / 2.0 - g(x)) * res / (displayYRange))
+    except:
+        y = INF
+    gx += [y]
+    if i > 0:
+        if gx[i-1] < 2 * res and gx[i-1] >= -res and gx[i] < 2 * res and gx[i] >= -res:
+            draw.line([(i-1,gx[i-1]), (i,gx[i])], fill='red', width=lineWidth)
 
 im.save('image.png')
